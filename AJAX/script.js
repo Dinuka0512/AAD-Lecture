428a2c97b8ab1461cb79d9eaeb8c7a429b919500
Dinuka Lakmal <Dinuka0512@gmail.com>
2025-05-07 15:16:51 +0530

    created when click load the data for table

--- AJAX/script.js
+++ AJAX/script.js
@@ -3,7 +3,7 @@ function btnClick(){
     const http = new XMLHttpRequest();
     console.log(http.readyState, "After the XMLHttpRequest object created");
 
-    http.open("GET", "db.txt");
+    http.open("GET", "database.json");
     console.log(http.readyState, "After the XMLHttpRequest Object opened");
     
     http.send();
@@ -12,7 +12,37 @@ function btnClick(){
     http.onreadystatechange = function(){
         console.log(http.readyState, "Inside On ready stage change event");
         console.log(http.responseText);
-        document.getElementById("paragraph").innerText = http.responseText;
+
+        if(http.readyState === 4){
+            // Assuming http.responseText is your JSON response string
+            let response = JSON.parse(http.responseText);
+
+            // document.getElementById("table").innerText = response.frist;
+            // // document.getElementById("lname").innerText = response.last;
+            // // document.getElementById("age").innerText = response.age;
+
+            let newRow = document.createElement("tr");
+            let fname = document.createElement("td");
+            fname.innerText = response.frist;
+
+            let lName = document.createElement("td");
+            lName.innerText = response.last;
+
+            let age = document.createElement("td");
+            age.innerText = response.age;
+
+            newRow.appendChild(fname);
+            newRow.appendChild(lName);
+            newRow.appendChild(age);
+
+            console.log(fname);
+            console.log(lName);
+            console.log(age);
+
+            console.log(newRow)
+
+            document.getElementById("tbody").append(newRow);
+        }
     }
 
     console.log("Request Finnished");
